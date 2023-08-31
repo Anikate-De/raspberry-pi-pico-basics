18ca89bcb0830514d035f8f5b709c9ee01ab48f6
Anikate-De <anikate1de@gmail.com>
2023-08-31 18:07:56 +0530

    added esc test; added mpu6050 tester code

---
+++ esc_test.py
@@ -0,0 +1,34 @@
+from machine import ADC, PWM, Pin
+
+esc = PWM(Pin(0))
+pot = ADC(26)
+
+esc.freq(50)
+
+def rangify(value):
+    init_start = 0
+    init_end = 65535
+
+    final_start = 3700 # 2293
+    final_end = 4200 #6553
+
+    init_range = init_end - init_start
+    final_range = final_end - final_start
+
+    ratio = final_range / init_range
+
+    distance = value - init_start
+    new_value = distance * ratio + final_start
+
+    return int(new_value)
+    
+
+while True:
+    pot_val = pot.read_u16()
+    duty = rangify(pot_val)
+    print("Potentiometer value: ", pot_val, ' Duty: ', duty)
+    esc.duty_u16(duty)
+
+
+
+
---
+++ mpu_test.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+from machine import I2C, Pin
+
+from imu import MPU6050
+
+i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=400000)
+imu = MPU6050(i2c)
+
+while True:
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    gx=round(imu.gyro.x)
+    gy=round(imu.gyro.y)
+    gz=round(imu.gyro.z)
+    tem=round(imu.temperature,2)
+    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
